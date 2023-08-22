641c2c3abedbc4c37c974764351189c048748b4f
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2023-08-22 10:22:27 -0300

    contador de palavras com frases do usuário

---
+++ main.py
@@ -0,0 +1,8 @@
+def contador_de_palavras(frase):
+    palavras = frase.split()
+    return len(palavras)
+
+frase_usuario = input("Digite uma frase")
+quantidade_palavras = contador_de_palavras(frase_usuario)
+print(f"A frase possui {quantidade_palavras} palavra(as).")
+
